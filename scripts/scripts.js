0aa86cb5ed83aeb0d8793dca0fe725b5430754e6
Victor <victorshiira4869@gmail.com>
2023-01-11 11:29:14 -0300

    Add an effect on the grid divs to change backgroundColor on hover

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,6 +4,11 @@ for (let i = 0; i < 16; i++) {
   for (let x = 0; x < 16; x++) {
     const square = document.createElement("div");
     square.classList.add("square");
+
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+
     container.appendChild(square);
   }
 }
