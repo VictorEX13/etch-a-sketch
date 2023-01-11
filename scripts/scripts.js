6db09d6b952673bacf5f062d2330eaa3f26ad8a2
Victor <victorshiira4869@gmail.com>
2023-01-11 14:22:56 -0300

    Create function to resize the grid

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,40 @@
 const container = document.querySelector(".container");
+const resize = document.querySelector(".resize");
 
-for (let i = 0; i < 16; i++) {
-  for (let x = 0; x < 16; x++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
+function createSquare() {
+  const square = document.createElement("div");
+  square.classList.add("square");
 
-    square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "black";
-    });
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "black";
+  });
 
-    container.appendChild(square);
+  container.appendChild(square);
+}
+
+function deleteGrid(element) {
+  element.replaceChildren();
+}
+
+function createNewGrid(size) {
+  if (size > 0 && size <= 100) {
+    deleteGrid(container);
+
+    for (let i = 0; i < size; i++) {
+      for (let x = 0; x < size; x++) {
+        createSquare();
+      }
+    }
+
+    container.style["grid-template-columns"] = `repeat(${size}, 1fr)`;
+    container.style["grid-template-rows"] = `repeat(${size}, 1fr)`;
+  } else {
+    alert("Not a valid size!");
   }
 }
+
+resize.addEventListener("click", () => {
+  createNewGrid(parseInt(prompt("Type the new grid's size (from 1 to 100): ")));
+});
+
+createNewGrid(16);
